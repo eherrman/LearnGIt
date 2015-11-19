a1f3c42651815fca013f8bb9a82332a8469000ef
Elise Herrmannsfeldt <Elise@Elises-MacBook-Pro.local>
2015-11-18 21:19:42 -0800

    Commit code 1 and code 2 but NAAAHT code 3

---
+++ code2.c
@@ -0,0 +1,46 @@
+number main
+	MY NAME IS ROSHAN AND I AM ORIGINAL HAHAHA SUCK IT LOSERZZZZZ
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+number main
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+	lololol
+	lololofdsjafkdsljfdsalkfh
+	wow so code!!!!
+}
